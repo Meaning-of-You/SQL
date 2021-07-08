30ebe0bdb7a9bc4681b334789c26f0f727f2a6df
KIMYUMIN <ymlmj@naver.com>
2021-07-08 23:47:01 +0900

    자동차 매출 데이터 분석

--- 자동차_매출_데이터_분석.sql
+++ 자동차_매출_데이터_분석.sql
@@ -79,5 +79,55 @@ GROUP BY 1;
 
 SELECT A.customerNumber, A.orderDate, B.customerNumber, B.orderDate
 FROM classicmodels.orders A
-LEFT JOIN classicmodels.oreders B
+LEFT JOIN classicmodels.orders B
 ON A.customerNumber = B.customerNumber AND substr(A.orderDate,1,4) = substr(B.orderDate,1,4) -1;
+
+SELECT C.country, substr(A.orderDate,1,4) YY,
+count(distinct A.customerNumber) BU_1,
+count(distinct B.customerNumber) BU_2,
+count(distinct B.customerNumber)/count(distinct A.customerNumber) Rate
+FROM classicmodels.orders A
+LEFT JOIN classicmodels.orders B
+ON A.customerNumber = B.customerNumber AND substr(A.orderDate,1,4) = substr(B.orderDate,1,4) -1
+LEFT JOIN classicmodels.customers C
+ON A.customerNumber = C.customerNumber
+GROUP BY 1, 2
+ORDER BY 2;
+
+CREATE TABLE classicmodels.product_sales
+SELECT D.productName, sum(quantityordered*priceeach) Sales
+FROM classicmodels.orders A
+LEFT JOIN classicmodels.customers B
+ON A.customerNumber = B.customerNumber
+LEFT JOIN classicmodels.orderdetails C
+ON A.orderNumber = C.orderNumber
+LEFT JOIN classicmodels.products D
+ON C.productCode = D.productCode
+WHERE B.country = 'USA'
+GROUP BY 1;
+
+SELECT * 
+FROM (SELECT *, row_number() OVER(ORDER BY Sales DESC) RNK
+	  FROM classicmodels.product_sales) A
+WHERE RNK <=5
+ORDER BY RNK;
+
+CREATE TABLE classicmodels.churn_list AS
+SELECT CASE WHEN DIFF >= 90 THEN 'CHURN' ELSE 'NON-CHURN' END CHURN_TYPE, customerNumber
+FROM (SELECT customerNumber, max_order, '2005-06-01' END_PONIT,
+	  datediff('2005-06-01',max_order) DIFF
+	  FROM (SELECT customerNumber, max(orderDate) max_order
+			FROM classicmodels.orders
+			GROUP BY 1) 
+	  BASE) BASE;
+      
+SELECT D.churn_type, C.productline, count(distinct B.customerNumber) BU
+FROM classicmodels.orderdetails A
+LEFT JOIN classicmodels.orders B
+ON A.orderNumber = B.orderNumber
+LEFT JOIN classicmodels.products C
+ON A.productCode = C.productCode
+LEFT JOIN classicmodels.churn_list D
+ON B.customerNumber = D.customerNumber
+GROUP BY 1, 2
+ORDER BY 1, 3 DESC;
